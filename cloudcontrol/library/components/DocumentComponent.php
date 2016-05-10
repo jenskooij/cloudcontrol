7ae7408f07868fe3c4f2df6f06234389eb1c2d46
Jens Kooij <jens.kooij@incentro.com>
2016-05-10 13:14:23 +0200

    Added a parameter to select the parameter for the found document

--- cloudcontrol/library/components/DocumentComponent.php
+++ cloudcontrol/library/components/DocumentComponent.php
@@ -7,24 +7,33 @@ namespace library\components {
 	 * Class DocumentComponent
 	 *
 	 * Has optional parameter `folder` to prefix the relative url with a folder
+	 * Has optional parameter `document` to select a given document
+	 * Has optional parameter `documentParameterName` to select the parametername to be used
+	 * 		to set the found document to.
 	 *
 	 * @package library\components
 	 */
 	class DocumentComponent extends BaseComponent
 	{
+		protected $documentParameterName = 'document';
+
 		public function run(Storage $storage)
 		{
 			parent::run($storage);
 
+			if (isset($this->parameters['documentParameterName'])) {
+				$this->documentParameterName = $this->parameters['documentParameterName'];
+			}
+
 			if ($this->matchedSitemapItem->regex == false) {
 				if (isset($this->parameters['document'])) {
-					$this->parameters['document'] = $storage->getDocumentBySlug($this->parameters['document']);
+					$this->parameters[$this->documentParameterName] = $storage->getDocumentBySlug($this->parameters['document']);
 				} else {
 					throw new \Exception('When not using a regex, you need to set the parameter `document` with the path to the document in this sitemap item: ' . $this->matchedSitemapItem->title);
 				}
 			} else {
 				if (isset($this->parameters['document'])) {
-					$this->parameters['document'] = $storage->getDocumentBySlug($this->parameters['document']);
+					$this->parameters[$this->documentParameterName] = $storage->getDocumentBySlug($this->parameters['document']);
 				} else {
 					$relativeDocumentUri = current($this->matchedSitemapItem->matches[1]);
 					if (isset($this->parameters['folder'])) {
@@ -43,7 +52,7 @@ namespace library\components {
 					if ($document->state != 'published') {
 						throw new \Exception('Found document is unpublished.');
 					}
-					$this->parameters['document'] = $document;
+					$this->parameters[$this->documentParameterName] = $document;
 				}
 			}
 		}
