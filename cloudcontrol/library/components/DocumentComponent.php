61a0d8f2f94c611cd9d2d6cde755a131c801267f
Jens Kooij <jenskooij1988@gmail.com>
2016-05-10 22:22:22 +0200

    Refactored DocumentComponent to decomplexify

--- cloudcontrol/library/components/DocumentComponent.php
+++ cloudcontrol/library/components/DocumentComponent.php
@@ -17,52 +17,113 @@ namespace library\components {
 	{
 		protected $documentParameterName = 'document';
 
+		/**
+		 * @param Storage $storage
+		 * @return mixed|void
+		 * @throws \Exception
+		 */
 		public function run(Storage $storage)
 		{
 			parent::run($storage);
 
+			$this->checkParameters();
+
+			if ($this->matchedSitemapItem === null) { // If no sitemapitem, its an application component
+				$this->runLikeApplicationComponent();
+			} else {
+				$this->runLikeRegularComponent();
+			}
+		}
+
+		/**
+		 * Checks to see if any parameters were defined in the cms and acts according
+		 */
+		private function checkParameters()
+		{
 			if (isset($this->parameters['documentParameterName'])) {
 				$this->documentParameterName = $this->parameters['documentParameterName'];
 			}
+		}
 
-			if ($this->matchedSitemapItem === null) { // If no sitemapitem, its an application component
-				if (isset($this->parameters['document'])) {
-					$this->parameters[$this->documentParameterName] = $storage->getDocumentBySlug($this->parameters['document']);
-				} else {
-					throw new \Exception('When used as application component, you need to specify a document.');
-				}
+		/**
+		 * Run as application component
+		 *
+		 * @throws \Exception
+		 */
+		private function runLikeApplicationComponent()
+		{
+			if (isset($this->parameters['document'])) {
+				$this->parameters[$this->documentParameterName] = $this->storage->getDocumentBySlug($this->parameters['document']);
 			} else {
-				if ($this->matchedSitemapItem->regex == false) {
-					if (isset($this->parameters['document'])) {
-						$this->parameters[$this->documentParameterName] = $storage->getDocumentBySlug($this->parameters['document']);
-					} else {
-						throw new \Exception('When not using a regex, you need to set the parameter `document` with the path to the document in this sitemap item: ' . $this->matchedSitemapItem->title);
-					}
-				} else {
-					if (isset($this->parameters['document'])) {
-						$this->parameters[$this->documentParameterName] = $storage->getDocumentBySlug($this->parameters['document']);
-					} else {
-						$relativeDocumentUri = current($this->matchedSitemapItem->matches[1]);
-						if (isset($this->parameters['folder'])) {
-							if (substr($this->parameters['folder'], -1) !== '/') {
-								$this->parameters['folder'] = $this->parameters['folder'] . '/';
-							}
-							$relativeDocumentUri = $this->parameters['folder'] . $relativeDocumentUri;
-						}
-
-						$document = $storage->getDocumentBySlug($relativeDocumentUri);
-
-						if ($document->type == 'folder') {
-							throw new \Exception('The found document is a folder.');
-						}
-
-						if ($document->state != 'published') {
-							throw new \Exception('Found document is unpublished.');
-						}
-						$this->parameters[$this->documentParameterName] = $document;
-					}
-				}
+				throw new \Exception('When used as application component, you need to specify a document.');
 			}
+		}
+
+		/**
+		 * Run as regular component
+		 *
+		 * @throws \Exception
+		 */
+		private function runLikeRegularComponent()
+		{
+			if ($this->matchedSitemapItem->regex == false) {
+				$this->runWithoutRegex();
+			} else {
+				$this->runWithRegex();
+			}
+		}
+
+		/**
+		 * Run without regex
+		 *
+		 * @throws \Exception
+		 */
+		private function runWithoutRegex()
+		{
+			if (isset($this->parameters['document'])) {
+				$this->runByDocumentParameter();
+			} else {
+				throw new \Exception('When not using a regex, you need to set the parameter `document` with the path to the document in this sitemap item: ' . $this->matchedSitemapItem->title);
+			}
+		}
+
+		/**
+		 * Run with regex
+		 *
+		 * @throws \Exception
+		 */
+		private function runWithRegex()
+		{
+			if (isset($this->parameters['document'])) {
+				$this->runByDocumentParameter();
+			} else {
+				$relativeDocumentUri = current($this->matchedSitemapItem->matches[1]);
+				if (isset($this->parameters['folder'])) {
+					if (substr($this->parameters['folder'], -1) !== '/') {
+						$this->parameters['folder'] = $this->parameters['folder'] . '/';
+					}
+					$relativeDocumentUri = $this->parameters['folder'] . $relativeDocumentUri;
+				}
+
+				$document = $this->storage->getDocumentBySlug($relativeDocumentUri);
+
+				if ($document->type == 'folder') {
+					throw new \Exception('The found document is a folder.');
+				}
+
+				if ($document->state != 'published') {
+					throw new \Exception('Found document is unpublished.');
+				}
+				$this->parameters[$this->documentParameterName] = $document;
+			}
+		}
+
+		/**
+		 * Run using the given `document` parameter
+		 */
+		private function runByDocumentParameter()
+		{
+			$this->parameters[$this->documentParameterName] = $this->storage->getDocumentBySlug($this->parameters['document']);
 		}
 	}
 }
