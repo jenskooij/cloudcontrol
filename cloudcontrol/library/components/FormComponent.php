92a637f29dc2d2b24867db1837989deba8c953fd
Jens Kooij <jens.kooij@incentro.com>
2017-01-30 15:29:29 +0100

    Minor refactor on FormComponent to lessen complexity

--- cloudcontrol/library/components/FormComponent.php
+++ cloudcontrol/library/components/FormComponent.php
@@ -7,248 +7,327 @@ use library\storage\Storage;
 
 class FormComponent Extends BaseComponent
 {
-    /**
-     * @var null|string
-     */
-    protected $documentType = null;
-    /**
-     * @var null|string
-     */
-    protected $responseFolder = null;
-    /**
-     * @var string
-     */
-    protected $subTemplate = 'cms/documents/document-form-form';
-    /**
-     * @var string
-     */
-    protected $formParameterName = 'form';
-    /**
-     * @var string
-     */
-    protected $thankYouMessage = 'Thank you for sending us your response.';
+	const GET_PARAMETER_PATH = 'path';
 
-    /**
-     * @var bool
-     */
-    protected $submitOncePerSession = false;
+	const PARAMETER_CMS_PREFIX = 'cmsPrefix';
+	const PARAMETER_DOCUMENT_TYPE = 'documentType';
+	const PARAMETER_DOCUMENT_TYPES = 'documentTypes';
+	const PARAMETER_FORM_ID = 'formId';
+	const PARAMETER_FORM_PARAMETER_NAME = 'formParameterName';
+	const PARAMETER_HIDE_TITLE_AND_STATE = 'hideTitleAndState';
+	const PARAMETER_RESPONSE_FOLDER = 'responseFolder';
+	const PARAMETER_SMALLEST_IMAGE = 'smallestImage';
+	const PARAMETER_SUBMIT_ONCE_PER_SESSION = 'submitOncePerSession';
+	const PARAMETER_SUB_TEMPLATE = 'subTemplate';
+	const PARAMETER_THANK_YOU_MESSAGE = 'thankYouMessage';
 
-    /**
-     * @var string
-     */
-    private $formId;
-    /**
-     * @var null|string
-     */
-    private $getPathBackup = null;
+	const SESSION_PARAMETER_CLOUDCONTROL = 'cloudcontrol';
+	const SESSION_PARAMETER_FORM_COMPONENT = 'FormComponent';
+	/**
+	 * @var null|string
+	 */
+	protected $documentType = null;
+	/**
+	 * @var null|string
+	 */
+	protected $responseFolder = null;
+	/**
+	 * @var string
+	 */
+	protected $subTemplate = 'cms/documents/document-form-form';
+	/**
+	 * @var string
+	 */
+	protected $formParameterName = 'form';
+	/**
+	 * @var string
+	 */
+	protected $thankYouMessage = 'Thank you for sending us your response.';
 
-    /**
-     * @var null|\stdClass
-     */
-    private $userSessionBackup = null;
+	/**
+	 * @var bool
+	 */
+	protected $submitOncePerSession = false;
 
-    /**
-     * @param Storage $storage
-     * @return void
-     * @throws \Exception
-     */
-    public function run(Storage $storage)
-    {
-        parent::run($storage);
+	/**
+	 * @var string
+	 */
+	private $formId;
+	/**
+	 * @var null|string
+	 */
+	private $getPathBackup = null;
 
-        $this->checkParameters();
+	/**
+	 * @var null|\stdClass
+	 */
+	private $userSessionBackup = null;
 
-        if ($this->documentType === null || $this->responseFolder === null) {
-            throw new \Exception('Parameters `documentType` and `responseFolder` are required for usage with this form');
-        }
+	/**
+	 * @param Storage $storage
+	 *
+	 * @return void
+	 * @throws \Exception
+	 */
+	public function run(Storage $storage)
+	{
+		parent::run($storage);
+		$this->checkParameters();
+		$this->checkRequiredParameters();
+		$this->setFormId();
+		$this->initialize($storage);
+		$this->checkSubmit($storage);
+	}
 
-        $this->setFormId();
-        $this->initialize($storage);
-        $this->checkSubmit($storage);
-    }
+	/**
+	 * @param null|Application $application
+	 *
+	 * @throws \Exception
+	 */
+	public function render($application = null)
+	{
+		$request = $this->setPathBackup($get);
+		$form = $this->renderTemplate($this->subTemplate);
+		$this->resetPathBackup($get, $request);
+		$this->setFormParameter($form);
 
-    /**
-     * @param null|Application $application
-     * @throws \Exception
-     */
-    public function render($application = null)
-    {
-        $request = $this->request;
-        if (isset($request::$get['path'])) {
-            $this->getPathBackup = $request::$get['path'];
-        }
-        $request::$get['path'] = $this->responseFolder;
-        $form = $this->renderTemplate($this->subTemplate);
-        if ($this->getPathBackup !== null) {
-            $request::$get['path'] = $this->getPathBackup;
-        } else {
-            unset($request::$get['path']);
-        }
-        if ($this->isFormSubmitted($this->request) || $this->isSubmitAllowed() === false) {
-            $this->parameters[$this->formParameterName] = '<a name="' . $this->formId . '"></a>' . $this->thankYouMessage;
-        } else {
-            $this->parameters[$this->formParameterName] = $form;
-        }
+		parent::render($application);
+	}
 
-        parent::render($application);
-    }
+	/**
+	 * Checks if parameters were given in the CMS configuration and
+	 * sets them to their respective fields
+	 */
+	private function checkParameters()
+	{
+		$this->checkDocumentTypeParameter();
+		$this->checkResponseFolderParameter();
+		$this->checkSubTemplateParameter();
+		$this->checkFormParameterNameParameter();
+		$this->checkThankYouMessageParameter();
+		$this->checkSubmitOncePerSessionParameter();
+	}
 
-    /**
-     * Checks if parameters were given in the CMS configuration and
-     * sets them to their respective fields
-     */
-    private function checkParameters()
-    {
-        if (isset($this->parameters['documentType'])) {
-            $this->documentType = $this->parameters['documentType'];
-            unset($this->parameters['documentType']);
-        }
+	/**
+	 * Sets variables needed for rendering the form template
+	 *
+	 * @param $storage
+	 */
+	private function initialize($storage)
+	{
+		$this->parameters[self::PARAMETER_SMALLEST_IMAGE] = $storage->getSmallestImageSet()->slug;
+		$this->parameters[self::PARAMETER_CMS_PREFIX] = '';
 
-        if (isset($this->parameters['responseFolder'])) {
-            $this->responseFolder = $this->parameters['responseFolder'];
-            unset($this->parameters['responseFolder']);
-        }
+		$this->parameters[self::PARAMETER_DOCUMENT_TYPE] = $this->storage->getDocumentTypeBySlug($this->documentType, true);
+		$this->parameters[self::PARAMETER_DOCUMENT_TYPES] = $this->storage->getDocumentTypes();
+		$this->parameters[self::PARAMETER_HIDE_TITLE_AND_STATE] = true;
+		$this->parameters[self::PARAMETER_FORM_ID] = $this->formId;
+	}
 
-        if (isset($this->parameters['subTemplate'])) {
-            $this->subTemplate = $this->parameters['subTemplate'];
-            unset($this->parameters['subTemplate']);
-        }
+	/**
+	 * If the form has been submitted, save the document
+	 * Calls $this->postSubmit() afterwards
+	 *
+	 * @param Storage $storage
+	 */
+	private function checkSubmit($storage)
+	{
+		if ($this->isFormSubmitted($this->request) && $this->isSubmitAllowed()) {
+			$postValues = $this->getPostValues($this->request);
+			$this->setUserSessionBackup();
+			$storage->addDocument($postValues);
+			$this->restoreUserSessionBackup();
+			$this->setSubmitToSession();
+			$this->postSubmit($postValues, $storage);
+		}
+	}
 
-        if (isset($this->parameters['formParameterName'])) {
-            $this->formParameterName = $this->parameters['formParameterName'];
-            unset($this->parameters['formParameterName']);
-        }
+	/**
+	 * Hook for derived classes to take actions after
+	 * submitting the form
+	 *
+	 * @param $postValues
+	 * @param $storage
+	 */
+	protected function postSubmit($postValues, $storage)
+	{
+	}
 
-        if (isset($this->parameters['thankYouMessage'])) {
-            $this->thankYouMessage = $this->parameters['thankYouMessage'];
-            unset($this->parameters['thankYouMessage']);
-        }
+	/**
+	 * Sets a unique id for this particular form, so it can recognize
+	 * it when a submit occurs
+	 */
+	private function setFormId()
+	{
+		if (isset($_SESSION[self::SESSION_PARAMETER_FORM_COMPONENT][$this->formParameterName][self::PARAMETER_FORM_ID])) {
+			$this->formId = $_SESSION[self::SESSION_PARAMETER_FORM_COMPONENT][$this->formParameterName][self::PARAMETER_FORM_ID];
+		} else {
+			$_SESSION[self::SESSION_PARAMETER_FORM_COMPONENT][$this->formParameterName][self::PARAMETER_FORM_ID] = (string)microtime(true);
+			$_SESSION[self::SESSION_PARAMETER_FORM_COMPONENT][$this->formParameterName]['submitted'] = false;
+			$this->formId = $_SESSION[self::SESSION_PARAMETER_FORM_COMPONENT][$this->formParameterName][self::PARAMETER_FORM_ID];
+		}
+	}
 
-        if (isset($this->parameters['submitOncePerSession'])) {
-            $this->submitOncePerSession = $this->parameters['submitOncePerSession'] === 'true' ? true : false;
-            unset($this->parameters['submitOncePerSession']);
-        }
-    }
+	/**
+	 * Checks if this form has been submitted
+	 *
+	 * @param $request
+	 *
+	 * @return bool
+	 */
+	private function isFormSubmitted($request)
+	{
+		return !empty($request::$post) && isset($request::$post[self::PARAMETER_FORM_ID]) && $request::$post[self::PARAMETER_FORM_ID] === $this->formId && isset($_SESSION[self::SESSION_PARAMETER_FORM_COMPONENT][$this->formParameterName][self::PARAMETER_FORM_ID]) && $_SESSION[self::SESSION_PARAMETER_FORM_COMPONENT][$this->formParameterName][self::PARAMETER_FORM_ID] === $this->formId;
+	}
 
-    /**
-     * Sets variables needed for rendering the form template
-     * @param $storage
-     */
-    private function initialize($storage)
-    {
-        $this->parameters['smallestImage'] = $storage->getSmallestImageSet()->slug;
-        $this->parameters['cmsPrefix'] = '';
+	/**
+	 *
+	 *
+	 * @param $request
+	 */
+	private function getPostValues($request)
+	{
+		$postValues = $request::$post;
+		$postValues[self::PARAMETER_DOCUMENT_TYPE] = $this->documentType;
+		$postValues[self::GET_PARAMETER_PATH] = $this->responseFolder;
+		$postValues['title'] = date('r') . ' - From: ' . $request::$requestUri;
 
-        $this->parameters['documentType'] = $this->storage->getDocumentTypeBySlug($this->documentType, true);
-        $this->parameters['documentTypes'] = $this->storage->getDocumentTypes();
-        $this->parameters['hideTitleAndState'] = true;
-        $this->parameters['formId'] = $this->formId;
-    }
+		return $postValues;
+	}
 
-    /**
-     * If the form has been submitted, save the document
-     * Calls $this->postSubmit() afterwards
-     *
-     * @param Storage $storage
-     */
-    private function checkSubmit($storage)
-    {
-        if ($this->isFormSubmitted($this->request) && $this->isSubmitAllowed()) {
-            $postValues = $this->getPostValues($this->request);
-            $this->setUserSessionBackup();
-            $storage->addDocument($postValues);
-            $this->restoreUserSessionBackup();
-            $this->setSubmitToSession();
-            $this->postSubmit($postValues, $storage);
-        }
-    }
+	/**
+	 * Temporarily stores the current user session in a backup variable
+	 * and sets a fake user instead
+	 */
+	private function setUserSessionBackup()
+	{
+		$this->userSessionBackup = isset($_SESSION[self::SESSION_PARAMETER_CLOUDCONTROL]) ? $_SESSION[self::SESSION_PARAMETER_CLOUDCONTROL] : null;
+		$fakeUser = new \stdClass();
+		$fakeUser->username = self::SESSION_PARAMETER_FORM_COMPONENT;
+		$_SESSION[self::SESSION_PARAMETER_CLOUDCONTROL] = $fakeUser;
+	}
 
-    /**
-     * Hook for derived classes to take actions after
-     * submitting the form
-     *
-     * @param $postValues
-     * @param $storage
-     */
-    protected function postSubmit($postValues, $storage)
-    {}
+	/**
+	 * Removes the fake user and restores the existing user
+	 * session if it was there
+	 */
+	private function restoreUserSessionBackup()
+	{
+		if ($this->userSessionBackup === null) {
+			unset($_SESSION[self::SESSION_PARAMETER_CLOUDCONTROL]);
+		} else {
+			$_SESSION[self::SESSION_PARAMETER_CLOUDCONTROL] = $this->userSessionBackup;
+		}
+	}
 
-    /**
-     * Sets a unique id for this particular form, so it can recognize
-     * it when a submit occurs
-     */
-    private function setFormId()
-    {
-        if (isset($_SESSION['FormComponent'][$this->formParameterName]['formId'])) {
-            $this->formId = $_SESSION['FormComponent'][$this->formParameterName]['formId'];
-        } else {
-            $_SESSION['FormComponent'][$this->formParameterName]['formId'] = (string) microtime(true);
-            $_SESSION['FormComponent'][$this->formParameterName]['submitted'] = false;
-            $this->formId = $_SESSION['FormComponent'][$this->formParameterName]['formId'];
-        }
-    }
+	private function setSubmitToSession()
+	{
+		$_SESSION[self::SESSION_PARAMETER_FORM_COMPONENT][$this->formParameterName]['submitted'] = true;
+	}
 
-    /**
-     * Checks if this form has been submitted
-     *
-     * @param $request
-     * @return bool
-     */
-    private function isFormSubmitted($request)
-    {
-        return !empty($request::$post) && isset($request::$post['formId']) && $request::$post['formId'] === $this->formId && isset($_SESSION['FormComponent'][$this->formParameterName]['formId']) && $_SESSION['FormComponent'][$this->formParameterName]['formId'] === $this->formId;
-    }
+	private function isSubmitAllowed()
+	{
+		if ($this->submitOncePerSession === true && $_SESSION[self::SESSION_PARAMETER_FORM_COMPONENT][$this->formParameterName]['submitted'] === true) {
+			return false;
+		} else {
+			return true;
+		}
+	}
 
-    /**
-     *
-     *
-     * @param $request
-     */
-    private function getPostValues($request)
-    {
-        $postValues = $request::$post;
-        $postValues['documentType'] = $this->documentType;
-        $postValues['path'] = $this->responseFolder;
-        $postValues['title'] = date('r') . ' - From: ' . $request::$requestUri;
-        return $postValues;
-    }
+	private function checkDocumentTypeParameter()
+	{
+		if (isset($this->parameters[self::PARAMETER_DOCUMENT_TYPE])) {
+			$this->documentType = $this->parameters[self::PARAMETER_DOCUMENT_TYPE];
+			unset($this->parameters[self::PARAMETER_DOCUMENT_TYPE]);
+		}
+	}
 
-    /**
-     * Temporarily stores the current user session in a backup variable
-     * and sets a fake user instead
-     */
-    private function setUserSessionBackup()
-    {
-        $this->userSessionBackup = isset($_SESSION['cloudcontrol']) ? $_SESSION['cloudcontrol'] : null;
-        $fakeUser = new \stdClass();
-        $fakeUser->username = 'FormComponent';
-        $_SESSION['cloudcontrol'] = $fakeUser;
-    }
+	private function checkResponseFolderParameter()
+	{
+		if (isset($this->parameters[self::PARAMETER_RESPONSE_FOLDER])) {
+			$this->responseFolder = $this->parameters[self::PARAMETER_RESPONSE_FOLDER];
+			unset($this->parameters[self::PARAMETER_RESPONSE_FOLDER]);
+		}
+	}
 
-    /**
-     * Removes the fake user and restores the existing user
-     * session if it was there
-     */
-    private function restoreUserSessionBackup()
-    {
-        if ($this->userSessionBackup === null) {
-            unset($_SESSION['cloudcontrol']);
-        } else {
-            $_SESSION['cloudcontrol'] = $this->userSessionBackup;
-        }
-    }
+	private function checkSubTemplateParameter()
+	{
+		if (isset($this->parameters[self::PARAMETER_SUB_TEMPLATE])) {
+			$this->subTemplate = $this->parameters[self::PARAMETER_SUB_TEMPLATE];
+			unset($this->parameters[self::PARAMETER_SUB_TEMPLATE]);
+		}
+	}
 
-    private function setSubmitToSession()
-    {
-        $_SESSION['FormComponent'][$this->formParameterName]['submitted'] = true;
-    }
+	private function checkFormParameterNameParameter()
+	{
+		if (isset($this->parameters[self::PARAMETER_FORM_PARAMETER_NAME])) {
+			$this->formParameterName = $this->parameters[self::PARAMETER_FORM_PARAMETER_NAME];
+			unset($this->parameters[self::PARAMETER_FORM_PARAMETER_NAME]);
+		}
+	}
 
-    private function isSubmitAllowed()
-    {
-        if ($this->submitOncePerSession === true && $_SESSION['FormComponent'][$this->formParameterName]['submitted'] === true) {
-            return false;
-        } else {
-            return true;
-        }
-    }
+	private function checkThankYouMessageParameter()
+	{
+		if (isset($this->parameters[self::PARAMETER_THANK_YOU_MESSAGE])) {
+			$this->thankYouMessage = $this->parameters[self::PARAMETER_THANK_YOU_MESSAGE];
+			unset($this->parameters[self::PARAMETER_THANK_YOU_MESSAGE]);
+		}
+	}
+
+	private function checkSubmitOncePerSessionParameter()
+	{
+		if (isset($this->parameters[self::PARAMETER_SUBMIT_ONCE_PER_SESSION])) {
+			$this->submitOncePerSession = $this->parameters[self::PARAMETER_SUBMIT_ONCE_PER_SESSION] === 'true';
+			unset($this->parameters[self::PARAMETER_SUBMIT_ONCE_PER_SESSION]);
+		}
+	}
+
+	/**
+	 * @throws \Exception
+	 */
+	private function checkRequiredParameters()
+	{
+		if ($this->documentType === null || $this->responseFolder === null) {
+			throw new \Exception('Parameters `documentType` and `responseFolder` are required for usage with this form');
+		}
+	}
+
+	/**
+	 * @return \library\cc\Request
+	 */
+	private function setPathBackup()
+	{
+		$request = $this->request;
+		if (isset($request::$get[self::GET_PARAMETER_PATH])) {
+			$this->getPathBackup = $request::$get[self::GET_PARAMETER_PATH];
+		}
+		$request::$get[self::GET_PARAMETER_PATH] = $this->responseFolder;
+
+		return $request;
+	}
+
+	/**
+	 * @param $get
+	 * @param $request
+	 */
+	private function resetPathBackup($get, $request)
+	{
+		if ($this->getPathBackup !== null) {
+			$request::$get[self::GET_PARAMETER_PATH] = $this->getPathBackup;
+		} else {
+			unset($request::$get[self::GET_PARAMETER_PATH]);
+		}
+	}
+
+	/**
+	 * @param $form
+	 */
+	private function setFormParameter($form)
+	{
+		if ($this->isFormSubmitted($this->request) || $this->isSubmitAllowed() === false) {
+			$this->parameters[$this->formParameterName] = '<a name="' . $this->formId . '"></a>' . $this->thankYouMessage;
+		} else {
+			$this->parameters[$this->formParameterName] = $form;
+		}
+	}
 }
