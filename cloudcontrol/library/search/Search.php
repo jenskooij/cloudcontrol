580be685428c749ec420dcd423d184caf7ad5f1a
Jens Kooij <jens.kooij@incentro.com>
2017-03-01 17:07:36 +0100

    Implemented basic search

--- cloudcontrol/library/search/Search.php
+++ cloudcontrol/library/search/Search.php
@@ -7,7 +7,22 @@
 
 namespace library\search;
 
-
+/**
+ * Class Search
+ * Formula:
+ * score(q,d)  =
+		queryNorm(q)
+		· coord(q,d)
+		· ∑ (
+			tf(t in d)
+			· idf(t)²
+			· t.getBoost()
+			· norm(t,d)
+		) (t in q)
+ *
+ *
+ * @package library\search
+ */
 class Search extends SearchDbConnected
 {
 	/**
@@ -22,40 +37,82 @@ class Search extends SearchDbConnected
 	public function getDocumentsForTokenizer(Tokenizer $tokenizer)
 	{
 		$this->tokenizer = $tokenizer;
-		$this->queryTokens();
+		$resultsPerTokens = $this->queryTokens();
+
+		$flatResults = $this->flattenResults($resultsPerTokens);
+		usort($flatResults, array($this, "scoreCompare"));
+
+		dump($flatResults);
 	}
 
 	private function queryTokens()
 	{
 		$tokenVector = $this->tokenizer->getTokenVector();
 		$tokens = array_keys($tokenVector);
+		$results = array();
 		foreach ($tokens as $token) {
-			$results = $this->getResultsForQuery($token);
-			dump($results);
-			dump('TODO implement search based on : score = tfForDoc("query") * idf("query")');
+			$results[$token] = $this->getResultsForToken($token);
 		}
+		return $results;
 	}
 
-	public function getResultsForQuery($query) {
+	public function getResultsForToken($token) {
 		$db = $this->getSearchDbHandle();
 		$sql = '
-			SELECT `tf`.`documentPath`, 
-				   `tf`.`frequency`,
-				   `idf`.`inverseDocumentFrequency`,
-				   (`tf`.`frequency` * `idf`.`inverseDocumentFrequency`) as `tfidfRank`
-			  FROM `term_frequency` as `tf`
-		 LEFT JOIN `inverse_document_frequency` as `idf`
-		 		ON `idf`.`term` = `tf`.`term`
-			 WHERE `tf`.`term` = :query
-		  ORDER BY `tfidfRank` DESC
+			SELECT (SUM(term_frequency.frequency) --TF
+				    * inverse_document_frequency.inverseDocumentFrequency -- IDF
+				    * SUM(term_frequency.termNorm) -- norm
+				    ) as score,
+				   SUM(term_frequency.frequency) as TF,
+				   inverse_document_frequency.inverseDocumentFrequency as IDF,
+				   SUM(term_frequency.termNorm) as norm,
+				   term_frequency.documentPath
+			  FROM term_frequency
+		 LEFT JOIN inverse_document_frequency
+		 		ON inverse_document_frequency.term = term_frequency.term
+			 WHERE term_frequency.term = :query
+		  GROUP BY term_frequency.documentPath, term_frequency.term
+		  ORDER BY score DESC
 		';
 		if(!$stmt = $db->prepare($sql)) {
 			throw new \Exception('SQLite exception: <pre>' . print_r($db->errorInfo(), true) . '</pre> for SQL:<pre>' . $sql . '</pre>');
 		}
-		$stmt->bindValue(':query', $query);
+		$stmt->bindValue(':query', $token);
 		if (!$stmt->execute()) {
 			throw new \Exception('SQLite exception: <pre>' . print_r($db->errorInfo(), true) . '</pre> for SQL:<pre>' . $sql . '</pre>');
 		}
 		return $stmt->fetchAll(\PDO::FETCH_CLASS, 'stdClass');
+	}
+
+	/**
+	 * @param $resultsPerTokens
+	 *
+	 * @return array
+	 */
+	private function flattenResults($resultsPerTokens)
+	{
+		$finalResults = array();
+		foreach ($resultsPerTokens as $token => $resultPerToken) {
+			foreach ($resultPerToken as $result) {
+				if (isset($finalResults[$result->documentPath])) {
+					$finalResults[$result->documentPath]->score += $result->score;
+					$finalResults[$result->documentPath]->matchingTokens[] = $token;
+				} else {
+					$resultObj = new \stdClass();
+					$resultObj->documentPath = $result->documentPath;
+					$resultObj->matchingTokens = array($token);
+					$resultObj->score = floatval($result->score);
+					$finalResults[$result->documentPath] = $resultObj;
+				}
+			}
+		}
+		return $finalResults;
+	}
+
+	private function scoreCompare($a, $b) {
+		if ($a->score == $b->score) {
+			return 0;
+		}
+		return ($a->score > $b->score) ? -1 : 1;
 	}
 }
