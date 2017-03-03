7f9feb3820b32c73f2c65372cc288f015afdbf38
Jens Kooij <jens.kooij@incentro.com>
2017-03-03 15:40:34 +0100

    Added search template

---
+++ cloudcontrol/templates/cms/search.php
@@ -0,0 +1,10 @@
+<section class="search">
+	<h2><i class="fa fa-search"></i> Search</h2>
+	<nav class="actions">
+		<ul>
+			<li>
+				<a class="btn" href="<?=\library\cc\Request::$subfolders?><?=$cmsPrefix?>/search/update-index" title="Update Index">Update Index</a>
+			</li>
+		</ul>
+	</nav>
+</section>
