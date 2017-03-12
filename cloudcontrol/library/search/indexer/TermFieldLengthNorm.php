72e06e2de51f56132616f1c3334f99d496cb4ba4
Jens Kooij <jenskooij1988@gmail.com>
2017-03-12 22:58:21 +0100

    Refactored Term Field Length Norm calculation which optimizes indexing A LOT

--- cloudcontrol/library/search/indexer/TermFieldLengthNorm.php
+++ cloudcontrol/library/search/indexer/TermFieldLengthNorm.php
@@ -6,6 +6,7 @@
  */
 
 namespace library\search\indexer;
+use library\search\Indexer;
 
 
 /**
@@ -34,19 +35,11 @@ class TermFieldLengthNorm
 	public function execute()
 	{
 		$db = $this->dbHandle;
-
 		$db->sqliteCreateFunction('sqrt', 'sqrt', 1);
 		$sql = '
-			UPDATE term_frequency
-			   SET termNorm = (
-				   SELECT 1/sqrt(SUM(count)) as calcNorm
-					  FROM term_count
-				     WHERE term_count.documentPath = term_frequency.documentPath
-				       AND term_count.field = term_frequency.field
-				       AND term_count.term = term_frequency.term
-			      GROUP BY documentPath, field
-			  )
-			  
+		SELECT documentPath, field, COUNT(`count`) as termCount
+		  FROM term_count
+	  GROUP BY documentPath, field
 		';
 		$stmt = $db->prepare($sql);
 		if ($stmt === false) {
@@ -54,6 +47,42 @@ class TermFieldLengthNorm
 			$errorMsg = $errorInfo[2];
 			throw new \Exception('SQLite Exception: ' . $errorMsg . ' in SQL: <br /><pre>' . $sql . '</pre>');
 		}
-		$stmt->execute();
+		if (($stmt->execute()) === false) {
+			$errorInfo = $db->errorInfo();
+			$errorMsg = $errorInfo[2];
+			throw new \Exception('SQLite Exception: ' . $errorMsg . ' in SQL: <br /><pre>' . $sql . '</pre>');
+		}
+		$uniqueFieldsPerDocument = $stmt->fetchAll(\PDO::FETCH_OBJ);
+		$values = array();
+		$i = 0;
+		foreach ($uniqueFieldsPerDocument as $fieldRow) {
+			$values[] = 'UPDATE term_frequency SET termNorm = 1/sqrt(' . intval($fieldRow->termCount) . ') WHERE documentPath = ' . $db->quote($fieldRow->documentPath) . ' AND field = ' . $db->quote($fieldRow->field) . ';';
+			$i += 1;
+			if ($i >= Indexer::SQLITE_MAX_COMPOUND_SELECT) {
+				$this->executeUpdateTermNorm($values, $db);
+				$values = array();
+				$i = 0;
+			}
+		}
+		if (count($values) != 0) {
+			$this->executeUpdateTermNorm($values, $db);
+		}
+	}
+
+	/**
+	 * @param array $values
+	 * @param \PDO $db
+	 * @throws \Exception
+	 */
+	private function executeUpdateTermNorm($values, $db)
+	{
+		$sql  = 'BEGIN TRANSACTION;' . PHP_EOL;
+		$sql .= implode(PHP_EOL, $values) . PHP_EOL;
+		$sql .= 'COMMIT;';
+		if (($db->exec($sql)) === false) {
+			$errorInfo = $db->errorInfo();
+			$errorMsg = $errorInfo[2];
+			throw new \Exception('SQLite Exception: ' . $errorMsg . ' in SQL: <br /><pre>' . $sql . '</pre>');
+		}
 	}
 }
