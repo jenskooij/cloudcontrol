1cfb3a32cf99134f23e152e36c8e9057f376ec00
Jens Kooij <jens.kooij@incentro.com>
2017-03-22 17:12:06 +0100

    Changed the look and feel of published and unpublished states in documents overview

--- cloudcontrol/templates/cms/documents.php
+++ cloudcontrol/templates/cms/documents.php
@@ -38,9 +38,10 @@
 <div class="grid-box-10">
 	<h3>
 		<a class="btn documentTitle" href="<?=\library\cc\Request::$subfolders?><?=$cmsPrefix?>/documents/edit-document?slug=<?=$slugPrefix . $document->slug?>" title="Edit">
-			<i class="fa fa-file-text-o"></i> <?=$document->title?>
+			<i class="fa fa-file-text-o"></i>
+            <small class="state <?=strtolower($document->state)?>"><i class="fa <?=$document->state == 'published' ? 'fa-check-circle-o' : 'fa-times-circle-o' ?>"></i></small>
+            <?=$document->title?>
 		</a>
-		<small class="small state <?=strtolower($document->state)?>"><?=ucfirst($document->state)?></small>
         <? if ($document->unpublishedChanges) : ?><small class="small unpublished-changes">Unpublished Changes</small><? endif ?>
 		<small class="small documentType"><?=$document->documentType?></small>
 		<small class="small lastModified" title="<?=date('r', $document->lastModificationDate)?>">
@@ -61,7 +62,7 @@
         <a class="btn unpublish" title="Unpublish" href="<?=\library\cc\Request::$subfolders?><?=$cmsPrefix?>/documents/unpublish-document?slug=<?=$slugPrefix . $document->slug?>"><i class="fa fa-times"></i></a>
     <? endif ?>
 	<a class="btn" href="<?=\library\cc\Request::$subfolders?><?=$cmsPrefix?>/documents/edit-document?slug=<?=$slugPrefix . $document->slug?>" title="Edit"><i class="fa fa-pencil"></i></a>
-	<? if (strtolower($document->state) == 'unpublished' || !$document->unpublishedChanges) : ?>
+	<? if (strtolower($document->state) == 'unpublished') : ?>
 	<a onclick="return confirm('Are you sure you want to delete this item?');" class="btn error" href="<?=\library\cc\Request::$subfolders?><?=$cmsPrefix?>/documents/delete-document?slug=<?=$slugPrefix . $document->slug?>" title="Delete"><i class="fa fa-trash"></i></a>
     <? endif ?>
 </div>
