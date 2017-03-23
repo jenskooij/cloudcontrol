b91fd0de1c4f4178e0043e710d9608ade3fb46ed
Jens Kooij <jens.kooij@incentro.com>
2017-03-23 11:22:10 +0100

    Abstracted render functions to seperate files for readability

--- cloudcontrol/templates/cms/documents.php
+++ cloudcontrol/templates/cms/documents.php
@@ -1,3 +1,5 @@
+<? include('documents/function.renderDocument.php'); ?>
+<? include('documents/function.renderFolder.php'); ?>
 <script>window.onload=function(){History.init();History.replaceState(null, 'Cloud Control CMS', '/<?=$request::$subfolders . $cmsPrefix?>/documents?path=/');};</script>
 <section class="documents">
 	<h2><i class="fa fa-file-text-o"></i> Documents</h2>
@@ -29,73 +31,7 @@
 						<?renderFolder($document, $cmsPrefix, '', true);?>
 					<? endif ?>
 				</li>
-
 			<? endforeach ?>
 		</ul>
 	<? endif ?>
 </section>
-<? function renderDocument($document, $cmsPrefix, $slugPrefix = '') {?>
-<div class="grid-box-10">
-	<h3>
-		<a class="btn documentTitle" href="<?=\library\cc\Request::$subfolders?><?=$cmsPrefix?>/documents/edit-document?slug=<?=$slugPrefix . $document->slug?>" title="Edit">
-			<i class="fa fa-file-text-o"></i>
-            <small class="state <?=strtolower($document->state)?>"><i class="fa <?=$document->state == 'published' ? 'fa-check-circle-o' : 'fa-times-circle-o' ?>"></i></small>
-            <?=$document->title?>
-		</a>
-        <? if ($document->unpublishedChanges) : ?><small class="small unpublished-changes">Unpublished Changes</small><? endif ?>
-		<small class="small documentType"><?=$document->documentType?></small>
-		<small class="small lastModified" title="<?=date('r', $document->lastModificationDate)?>">
-			<span class="label">Modified:</span>
-			<?=\library\cc\StringUtil::timeElapsedString($document->lastModificationDate)?>
-		</small>
-		<small class="small lastModifiedBy">
-			<span class="label">By:</span>
-			<?=$document->lastModifiedBy?>
-		</small>
-	</h3>
-</div>
-<div class="documentActions grid-box-2">
-    <? if (strtolower($document->state) == 'unpublished' || $document->unpublishedChanges) : ?>
-        <a class="btn publish" title="Publish" href="<?=\library\cc\Request::$subfolders?><?=$cmsPrefix?>/documents/publish-document?slug=<?=$slugPrefix . $document->slug?>"><i class="fa fa-check"></i></a>
-    <? endif ?>
-    <? if (strtolower($document->state) == 'published') : ?>
-        <a class="btn unpublish" title="Unpublish" href="<?=\library\cc\Request::$subfolders?><?=$cmsPrefix?>/documents/unpublish-document?slug=<?=$slugPrefix . $document->slug?>"><i class="fa fa-times"></i></a>
-    <? endif ?>
-	<a class="btn" href="<?=\library\cc\Request::$subfolders?><?=$cmsPrefix?>/documents/edit-document?slug=<?=$slugPrefix . $document->slug?>" title="Edit"><i class="fa fa-pencil"></i></a>
-	<? if (strtolower($document->state) == 'unpublished') : ?>
-	<a onclick="return confirm('Are you sure you want to delete this item?');" class="btn error" href="<?=\library\cc\Request::$subfolders?><?=$cmsPrefix?>/documents/delete-document?slug=<?=$slugPrefix . $document->slug?>" title="Delete"><i class="fa fa-trash"></i></a>
-    <? endif ?>
-</div>
-<?}?>
-<? function renderFolder($document, $cmsPrefix, $slugPrefix ='', $root = false) {?>
-<div class="grid-box-8">
-	<h3>
-		<a class="btn documentTitle openFolder" data-slug="<?=$slugPrefix . $document->slug?>" title="Open">
-			<i class="fa fa-folder-o "></i> <?=$document->title?>
-		</a>
-	</h3>
-</div>
-<div class="documentActions grid-box-4">
-	<a class="btn" href="<?=\library\cc\Request::$subfolders?><?=$cmsPrefix?>/documents/edit-folder?slug=<?=$slugPrefix . $document->slug?>" title="Edit"><i class="fa fa-pencil"></i></a>
-	<a onclick="return confirm('Are you sure you want to delete this item?');" class="btn error" href="<?=\library\cc\Request::$subfolders?><?=$cmsPrefix?>/documents/delete-folder?slug=<?=$slugPrefix . $document->slug?>" title="Delete"><i class="fa fa-trash"></i></a>
-</div>
-<ul class="documents grid-wrapper nested<?=$root ? ' root' : '' ?>">
-	<? foreach ($document->content as $subDocument) : ?>
-		<li class="grid-container">
-			<? if ($subDocument->type == 'document') : ?>
-				<?renderDocument($subDocument, $cmsPrefix, $slugPrefix . $document->slug . '/');?>
-			<? elseif ($subDocument->type == 'folder') : ?>
-				<?renderFolder($subDocument, $cmsPrefix, $slugPrefix . $document->slug . '/');?>
-			<? endif ?>
-		</li>
-	<? endforeach ?>
-	<? if (count($document->content) == 0) : ?>
-		<li class="grid-container">
-			<div class="grid-box-12">
-				<i class="fa fa-ellipsis-h empty"></i>
-				<i>Empty</i>
-			</div>
-		</li>
-	<? endif ?>
-</ul>
-<?}?>
