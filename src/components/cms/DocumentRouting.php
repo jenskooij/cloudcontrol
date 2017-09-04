de6037da8c8436f9f285c0bac879ee4159ba61c6
Jens Kooij <jens.kooij@incentro.com>
2017-09-04 15:50:57 +0200

    Removed unused use statement

--- src/components/cms/DocumentRouting.php
+++ src/components/cms/DocumentRouting.php
@@ -10,7 +10,6 @@ namespace CloudControl\Cms\components\cms;
 
 use CloudControl\Cms\cc\Request;
 use CloudControl\Cms\components\cms\document\FolderRouting;
-use CloudControl\Cms\components\cms\document\ValuelistRouting;
 use CloudControl\Cms\components\CmsComponent;
 use CloudControl\Cms\search\Search;
 use CloudControl\Cms\storage\Document;
