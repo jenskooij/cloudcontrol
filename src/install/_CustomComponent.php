3896e90ac3d01c7725d70f8ed5729e02c433abd8
Jens Kooij <jens.kooij@incentro.com>
2017-09-12 10:51:53 +0200

    Added default custom component

---
+++ src/install/_CustomComponent.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Custom Component
+ */
+
+namespace components;
+
+
+use CloudControl\Cms\components\BaseComponent;
+use CloudControl\Cms\storage\Storage;
+
+class CustomComponent extends BaseComponent
+{
+    /**
+     * @param Storage $storage
+     * @return void
+     */
+    public function run(Storage $storage)
+    {
+        parent::run($storage);
+        // TODO Start coding here
+    }
+}
