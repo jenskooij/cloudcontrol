539cfcd0083c7f61c55d943e12ab6a298a3b7d7d
Jens Kooij <jens.kooij@incentro.com>
2018-01-10 17:00:08 +0100

    Changed a bug where urls containing a filename would trigger the cli-server routing to try and serve a resource, which didnt exist

--- src/install/_index.php
+++ src/install/_index.php
@@ -3,6 +3,13 @@
  * Created by jensk on 15-8-2017.
  */
 require_once(__DIR__ . '/../vendor/autoload.php');
+if (php_sapi_name() === 'cli-server') {
+    if (preg_match('/\.(?:js|ico|txt|gif|jpg|jpeg|png|bmp|css|html|htm|php|pdf|exe|eot|svg|ttf|woff|ogg|mp3|xml|map|scss)$/', $_SERVER["REQUEST_URI"])) {
+        if (file_exists(__DIR__ . $_SERVER["REQUEST_URI"])) {
+            return false;    // serve the requested resource as-is.
+        }
+    }
+}
 // Error settings
 ini_set('display_errors', true);
 ini_set('error_reporting', E_ALL);
@@ -23,12 +30,6 @@ session_start();
 $rootDir = realpath(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR);
 $configPath = realpath($rootDir . DIRECTORY_SEPARATOR . 'config.json');
 \CloudControl\Cms\CloudControl::run($rootDir, $configPath);
-
-if (php_sapi_name() === 'cli-server') {
-    if (preg_match('/\.(?:js|ico|txt|gif|jpg|jpeg|png|bmp|css|html|htm|php|pdf|exe|eot|svg|ttf|woff|ogg|mp3|xml|map|scss)$/', $_SERVER["REQUEST_URI"])) {
-        return false;    // serve the requested resource as-is.
-    }
-}
 
 if (php_sapi_name() != "cli") {
     ob_end_flush();
